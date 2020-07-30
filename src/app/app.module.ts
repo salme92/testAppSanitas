dc273a32a16357f911ae6edc6318bea1adaf1b95
Alberto Salmerón <albertosalmerontapia@gmail.com>
2020-07-30 13:10:31 +0200

    Add ReactiveFormsModule in NgModule for search with pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ElementListComponent } from './element-list/element-list.component';
@@ -11,7 +12,8 @@ import { FilterIdPipe } from './pipes/filter-id.pipe';
     AppComponent,
     ElementListComponent,
     FilterIdPipe,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   imports: [
     BrowserModule
