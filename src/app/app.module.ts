da61335550112dd467cb68064ed106f70f06d307
Alberto Salmerón <albertosalmerontapia@gmail.com>
2020-07-30 11:24:10 +0200

    creating new component element-list and integrate into app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ElementListComponent } from './element-list/element-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ElementListComponent
   ],
   imports: [
     BrowserModule
